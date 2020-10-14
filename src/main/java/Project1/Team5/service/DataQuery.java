cc8f409fea0a6b2118cd99ae421bc4e7d8868f71
Ahmed Ruhul Quddos Joyon <ahjoyo@ttu.ee>
2020-10-14 13:29:47 +0300

    method created to call objects

--- src/main/java/Project1/Team5/service/DataQuery.java
+++ src/main/java/Project1/Team5/service/DataQuery.java
@@ -3,6 +3,7 @@ package Project1.Team5.service;
 import Project1.Team5.service.Dota.Player;
 import Project1.Team5.service.Dota.Response;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +12,13 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@EqualsAndHashCode
 @Data
 @Service
 public class DataQuery extends Response  {
 
 
     RestTemplate restTemplate = new RestTemplate();
-
-
-
-
-
 
     public DotaResponse dataQuery(){
 
@@ -30,8 +27,15 @@ public class DataQuery extends Response  {
        ResponseEntity<List<Player>> rateResponse = restTemplate.exchange("https://api.opendota.com/api/players/100616105/recentMatches?api_key=1d67e82f-c0f0-4e49-bf0d-7a4e2bc537e2",
                         HttpMethod.GET, null, new ParameterizedTypeReference<List<Player>>() {
                         });
-        List<Player> response = rateResponse.getBody();
-        //return response;
+        List<Player> players = rateResponse.getBody();
+        for (Player player: players
+             ) {
+            dotaResponse.setKills(player.getKills());
+
+        }
+
+        return dotaResponse;
+
 
 
 
--- src/main/java/Project1/Team5/service/Dota/Player.java
+++ src/main/java/Project1/Team5/service/Dota/Player.java
@@ -3,9 +3,11 @@ package Project1.Team5.service.Dota;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.awt.*;
 
+@EqualsAndHashCode
 @Data
 public class Player {
 
