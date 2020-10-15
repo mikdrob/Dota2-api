6f84cb0e876e012a6d87fab01e5b330ec2e34d4f
Shpetim Ibrani <timiibrani@gmail.com>
2020-10-16 01:54:47 +0300

    Add fields in JSON response

--- src/main/java/Project1/Team5/service/DotaResponse.java
+++ src/main/java/Project1/Team5/service/DotaResponse.java
@@ -7,8 +7,28 @@ import lombok.Data;
 @Data
 public class DotaResponse {
 
-    private int assists;
-    private int kills;
-    private int total_gold;
-    private int total_xp;
+    //TODO: Add constructor 
+
+    private String player_name;
+    private int solo_competitive_rank;
+    private int competitive_rank;
+    private int mmr_estimate;
+    private MostUsedHero most_used_hero = new MostUsedHero();
+    private LeastUsedHero least_used_hero = new LeastUsedHero();
+
+    @Data
+    class MostUsedHero {
+        private int hero_id;
+        private int matches_played;
+        private int matches_won;
+        private int win_percentage;
+    }
+
+    @Data
+    class LeastUsedHero {
+        private int hero_id;
+    }
+
+
 }
+
