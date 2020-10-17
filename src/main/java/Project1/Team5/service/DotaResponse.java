6241df58fa50f6ea1d9a7f8b4f602201c16b8bf0
Shpetim Ibrani <timiibrani@gmail.com>
2020-10-17 17:30:55 +0300

    Add JSON response objects & fields

--- src/main/java/Project1/Team5/service/DotaResponse.java
+++ src/main/java/Project1/Team5/service/DotaResponse.java
@@ -7,33 +7,46 @@ import lombok.Data;
 @Data
 public class DotaResponse {
 
-    //TODO: Add constructor
-
     private String player_name;
-    private int solo_competitive_rank;
-    private int competitive_rank;
-    private MMR_estimate mmr_estimate = new MMR_estimate();
-    private MostUsedHero most_used_hero = new MostUsedHero();
-    private LeastUsedHero least_used_hero = new LeastUsedHero();
+    private String solo_competitive_rank;
+    private String competitive_rank;
+    private int mmr_estimate;
+    private MostUsedHero most_used_hero;
+    private LeastUsedHero least_used_hero;
+
+    public DotaResponse(String player_name, String solo_competitive_rank, String competitive_rank, int mmr_estimate, MostUsedHero most_used_hero, LeastUsedHero least_used_hero) {
+        this.player_name = player_name;
+        this.solo_competitive_rank = solo_competitive_rank;
+        this.competitive_rank = competitive_rank;
+        this.mmr_estimate = mmr_estimate;
+        this.most_used_hero = most_used_hero;
+        this.least_used_hero = least_used_hero;
+    }
 
     @Data
+    static
     class MostUsedHero {
         private int hero_id;
-        private int matches_played;
-        private int matches_won;
-        private int win_percentage;
+        private long matches_played;
+        private long matches_won;
+        private double win_percentage;
+
+        public MostUsedHero(int hero_id, long matches_played, long matches_won, double win_percentage) {
+            this.hero_id = hero_id;
+            this.matches_played = matches_played;
+            this.matches_won = matches_won;
+            this.win_percentage = win_percentage;
+        }
     }
 
     @Data
+    static
     class LeastUsedHero {
-        private int hero_id;
-    }
+        public LeastUsedHero(int hero_id) {
+            this.hero_id = hero_id;
+        }
 
-    @Data
-    class MMR_estimate {
-        private int estimate;
-        private int stdDev;
-        private int n;
+        private int hero_id;
     }
 
 
