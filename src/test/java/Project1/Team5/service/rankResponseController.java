a73da2a7593499a4d6a377bc83bb590a2be1b082
Florian Shllaku <florian.shllaku@gmail.com>
2020-10-17 23:30:06 +0300

    answer question 6

--- src/test/java/Project1/Team5/service/rankResponseController.java
+++ src/test/java/Project1/Team5/service/rankResponseController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 class rankResponseController {
 
     @Autowired
