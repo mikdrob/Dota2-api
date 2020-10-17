a55fa87f22e7eb1ec77e770f40582e4b011cbe68
Shpetim Ibrani <timiibrani@gmail.com>
2020-10-17 17:30:00 +0300

    Add fields to parse from JSON response

--- src/main/java/Project1/Team5/service/Dota/Player.java
+++ src/main/java/Project1/Team5/service/Dota/Player.java
@@ -12,18 +12,51 @@ import java.util.List;
 @Data
 
 public class Player {
-    public String getSoloRank() { return solo_competative_rank; }
+    public String getSoloRank() { return solo_competitive_rank; }
 
-    public String getCompetativeRank() { return competative_rank; }
+    public String getCompetitiveRank() { return competitive_rank; }
 
-    public Object getMMREstimate() { return mmr_estimate; }
+    public MMR getMmr_estimate() {
+        return mmr_estimate;
+    }
 
-    @JsonProperty("solo_competative_rank")
-    private String solo_competative_rank;
+    public Profile getProfile() {
+        return profile;
+    }
+
+    @JsonProperty("solo_competitive_rank")
+    private String solo_competitive_rank;
 
     @JsonProperty("competitive_rank")
-    private String competative_rank;
+    private String competitive_rank;
 
     @JsonProperty("mmr_estimate")
-    private Object mmr_estimate;
+    private MMR mmr_estimate;
+
+    @JsonProperty("profile")
+    private Profile profile;
+
+    @Data
+    public class MMR {
+        public int getEstimate() {
+            return estimate;
+        }
+
+        @JsonProperty("estimate")
+        private int estimate;
+
+    }
+
+    @Data
+    public class Profile {
+        public String getName() {
+            return name;
+        }
+
+        @JsonProperty("name")
+        private String name;
+
+    }
+
 }
+
