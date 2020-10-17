d733c506f84c11ae8817e77e78eeb49806d9cfba
midrob <midrob@ttu.ee>
2020-10-17 18:04:52 +0300

    answer question5

--- src/main/java/a_theory/a_theory/question5/SpyVsMock.java
+++ src/main/java/a_theory/a_theory/question5/SpyVsMock.java
@@ -6,13 +6,14 @@ public class SpyVsMock {
 
     //todo p1
     // What is the difference between spying or mocking an object?
-    // Answer:
+    // Answer: While the former mocks an object the latter takes only a part of it and leaves the rest
+    // with its real implementation
 
     //todo p2
     // Name 1 benefit of spy over mock
-    // 1
+    // 1 for example we can modify small bit of API to test it
 
     //todo p3
     // Name 1 benefit of mock over spy
-    // 1
+    // 1 It is safe and we don't have to call external dependencies
 }
