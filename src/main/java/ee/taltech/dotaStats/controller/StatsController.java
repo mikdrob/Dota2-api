9d6259e6a173cd98cce0bbb33726056002863d3e
midrob <midrob@ttu.ee>
2020-10-22 16:11:07 +0300

    log responses and requests

--- src/main/java/ee/taltech/dotaStats/controller/StatsController.java
+++ src/main/java/ee/taltech/dotaStats/controller/StatsController.java
@@ -3,19 +3,22 @@ package ee.taltech.dotaStats.controller;
 
 import ee.taltech.dotaStats.service.DataQuery;
 import ee.taltech.dotaStats.service.DotaResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RequestMapping("/stats")
 @RestController
 public class StatsController {
 
     @GetMapping
     public DotaResponse index(@RequestParam String playerId) {
+        log.info("Player ID - " + playerId);
         DataQuery dataquery = new DataQuery();
+        log.info("response from https://api.opendota.com/api/players/" + dataquery.dataQuery(playerId));
         return dataquery.dataQuery(playerId);
     }
 
--- src/main/java/ee/taltech/dotaStats/exception/StatsException.java
+++ src/main/java/ee/taltech/dotaStats/exception/StatsException.java
@@ -1,11 +1,13 @@
 package ee.taltech.dotaStats.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+@Slf4j
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "unknown symbol")
 public class StatsException extends RuntimeException {
     public StatsException(String message) {
         super(message);
+        log.error("Caught exception " + message);
     }
 }
