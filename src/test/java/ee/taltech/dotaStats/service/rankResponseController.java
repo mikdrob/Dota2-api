0f40f858b6b089759cfdf8a3925a07b0fa71de2b
Florian Shllaku <florian.shllaku@gmail.com>
2020-10-24 21:19:22 +0300

    add ControllerMostUsedHero test

--- src/test/java/ee/taltech/dotaStats/service/rankResponseController.java
+++ src/test/java/ee/taltech/dotaStats/service/rankResponseController.java
@@ -15,9 +15,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 import static ee.taltech.dotaStats.service.DotaCalculation.*;
@@ -56,6 +58,26 @@ class rankResponseController {
         Map<Integer, Long> heroIDOccurrences = calculateHeroIDOCcurrences(Collections.singletonList(matches));;
 
         assertNotNull(heroIDOccurrences);
+
+    }
+
+    @Test
+    void ControllerMostUsedHero() {
+        ResponseEntity<Match> rateResponse = testRestTemplate.exchange("/stats?playerId=100616105&displayMatches=true", HttpMethod.GET, null, new ParameterizedTypeReference<Match>() {
+        });
+        Match matches = rateResponse.getBody();
+
+        ArrayList<Integer> heroIDList = new ArrayList<Integer>();
+
+        for (Match match : Collections.singletonList(matches)) {
+            heroIDList.add(match.getHero_id());
+        }
+
+        Map<Integer, Long> heroIDOccurrences = heroIDList.stream().collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        DotaResponse.MostUsedHero mostUsedHero = calculate_mostUsedHero(Collections.singletonList(matches), heroIDOccurrences);
+
+        assertNotNull(mostUsedHero);
 
     }
 
