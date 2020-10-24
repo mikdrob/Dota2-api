a52369ba7b560c9a7251700a65eba14967569059
midrob <midrob@ttu.ee>
2020-10-24 18:35:33 +0300

    restore removed theory

---
+++ src/main/java/a_theory/a_theory/question5/SpyVsMock.java
@@ -0,0 +1,19 @@
+package a_theory.a_theory.question5;
+
+public class SpyVsMock {
+
+    //todo "To spy or to mock, that is the question" Shakespeare, not used for Hamlet.
+
+    //todo p1
+    // What is the difference between spying or mocking an object?
+    // Answer: While the former mocks an object the latter takes only a part of it and leaves the rest
+    // with its real implementation
+
+    //todo p2
+    // Name 1 benefit of spy over mock
+    // 1 for example we can modify small bit of API to test it
+
+    //todo p3
+    // Name 1 benefit of mock over spy
+    // 1 It is safe and we don't have to call external dependencies
+}
