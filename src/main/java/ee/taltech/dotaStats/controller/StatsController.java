65fbaa4f275b3a79321738992ececcafe7f50810
Shpetim Ibrani <timiibrani@gmail.com>
2020-12-12 22:17:17 +0200

    Test CI/CD

--- src/main/java/ee/taltech/dotaStats/controller/StatsController.java
+++ src/main/java/ee/taltech/dotaStats/controller/StatsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RequestMapping("/stats")
+@RequestMapping({"/stats", "/stat2"})
 @Tag(name = "Player Stats", description = "Return player hero stats & rank based on matches")
 @RestController
 public class StatsController {
